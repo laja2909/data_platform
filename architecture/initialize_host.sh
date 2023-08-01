37a34f0551e26dbd6f576e59a84f64b00ad867d7
Laja2909 <lauri2909@gmail.com>
2023-08-02 00:04:30 +0300

    changes made, slow progress...

--- architecture/initialize_host.sh
+++ architecture/initialize_host.sh
@@ -1,14 +1,8 @@
 #!/bin/bash
 
-echo 'Shout: Initialize.sh script starts...'
-echo 'Shout: Initializing host machine.'
-
-echo 'Shout: install ansible and run playbook'
 sudo apt-get update
 sudo apt-get install -y python3-pip
 sudo -H pip3 install ansible==5.9.0
-
-echo 'Shout: ansible installed!'
 
 
 
